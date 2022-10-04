ae08bfbcdcaeab57c7b5a533b9291398ca80cdb9
ZhinkyPinky <jonathan.engstrom@gmail.com>
2022-10-04 15:16:07 +0200

    +started adding more apis

--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/businessLogicLayer/repositories/ICustomerRepository.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/businessLogicLayer/repositories/ICustomerRepository.java
@@ -3,6 +3,7 @@ package com.example.constructionappapi.services.businessLogicLayer.repositories;
 import com.example.constructionappapi.services.dataAccessLayer.entities.CustomerEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * A interface to function as a model for all the repositories
@@ -11,4 +12,8 @@ public interface ICustomerRepository {
     CustomerEntity createCustomer(CustomerEntity customer);
 
     List<CustomerEntity> getAllCustomers();
+
+    Optional<CustomerEntity> getCustomer(Long id);
+
+    void deleteCustomer(Long id);
 }
