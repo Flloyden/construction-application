acb6c576686f066dd70700c0382b32f9efd6fbf8
Adam Lloyd <lloyd.adam99@gmail.com>
2022-10-05 17:28:05 +0200

    färgändring

--- construction-app/client/src/components/CustomerRegister.js
+++ construction-app/client/src/components/CustomerRegister.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import ApiConnector from '../services/ApiConnector';
+import '../styles/CustomerRegister.css';
+import { FaPen } from 'react-icons/fa';
+import { ImCross } from 'react-icons/im';
+import { BsPerson } from 'react-icons/bs';
 
 const CustomerRegister = () => {
     // Declaring variables
@@ -25,15 +29,38 @@ const CustomerRegister = () => {
 
   return (
     <div className='container'>
-        <h1>Kundregister</h1>
-        <button onClick={() => navigate("/skapakund")}>Lägg till kund</button>
-        {!loading && (
-            <div>
-            {kunder.map((kunder) => (
-                <h4 key={kunder.id}>{kunder.name}</h4>
-            ))}
+        <div className='content'>
+            <div className='topContent'>
+                <input type="text" className="myInput" placeholder="Sök efter namn.." title="Type in a name"></input>
+                <button className='addClient' onClick={() => navigate("/skapakund")}><span className='newClientIcon'><BsPerson /></span> Ny kund</button>
             </div>
-        )}
+        <table className="styled-table">
+            <thead>
+                <tr>
+                    <th>Namn</th>
+                    <th>Adress</th>
+                    <th>Kommentar</th>
+                    <th>Skapad</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            {!loading && (
+                <tbody>
+                    {kunder.map((kunder) => (
+                        <tr key={kunder.id}>
+                            <td>{kunder.name}</td>
+                            <td>{kunder.address}</td>
+                            <td>Fönster</td>
+                            <td>2022-10-04</td>
+                            <td className='icons'><FaPen className='editIcon' /></td>
+                            <td className='icons'><ImCross className='removeIcon' /></td>
+                        </tr>
+                    ))}
+                </tbody>
+            )}
+        </table>
+        </div>
     </div>
   )
 }
