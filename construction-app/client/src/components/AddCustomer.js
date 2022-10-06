9885eaf7893e23f178259007096568504eb4b5e6
Adam Lloyd <lloyd.adam99@gmail.com>
2022-10-06 12:49:33 +0200

    Fixat så att det inte är null värde i databasen

--- construction-app/client/src/components/AddCustomer.js
+++ construction-app/client/src/components/AddCustomer.js
@@ -1,56 +1,90 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import ApiConnector from '../services/ApiConnector';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiConnector from "../services/ApiConnector";
 
 const AddCustomer = () => {
-    // Declare variables
-    const navigate = useNavigate();
-    const [kund, setKund] = useState ({
-        id: "",
-        name: ""
-    });
+  // Declare variables
+  const navigate = useNavigate();
+  const [kund, setKund] = useState({
+    id: "",
+    name: "",
+    address: "",
+    phoneNumber: "",
+    propertyDesignation: "",
+    socialSecurityNumber: "",
+  });
 
-const handleChange = (e) => {
+  const handleChange = (e) => {
     /**Gets the current input every keystroke */
     const value = e.target.value;
-    setKund({...kund, [e.target.name]: value},
-        {...kund,[e.target.phonenumber]:value},
-        {...kund,[e.target.address]:value},
-        {...kund,[e.target.fastighetsbeteckning]:value},
-        {...kund,[e.target.phonenumber]:value});
-}
+    setKund({
+      ...kund,
+      [e.target.name]: value,
+      [e.target.address]: value,
+      [e.target.phoneNumber]: value,
+      [e.target.propertyDesignation]: value,
+      [e.target.socialSecurityNumber]: value,
+    });
+  };
 
-const saveKund = (e) => {
+  const saveKund = (e) => {
     /**Saves the "kund" and navigates back to the register */
     e.preventDefault();
-    ApiConnector.saveKund(kund).then((response) => {
-        console.log(response)
-        navigate("/kundregister")
-    }).catch((error) => {
+    ApiConnector.saveKund(kund)
+      .then((response) => {
+        console.log(response);
+        navigate("/kundregister");
+      })
+      .catch((error) => {
         console.log(error);
-    })
-};
-         
+      });
+  };
+
   return (
-    <div className='container'>
-        <label>Namn: </label>
-        <input type="text" name='name' value={kund.name} onChange={(e) => handleChange(e)}></input>
+    <div className="container">
+      <label>Namn: </label>
+      <input
+        type="text"
+        name="name"
+        value={kund.name}
+        onChange={(e) => handleChange(e)}
+      ></input>
 
-        <label>Adress: </label>
-        <input type="text" name='address' value={kund.address} onChange={(e) => handleChange(e)}></input>
+      <label>Adress: </label>
+      <input
+        type="text"
+        name="address"
+        value={kund.address}
+        onChange={(e) => handleChange(e)}
+      ></input>
 
-        <label>Telefonnummer: </label>
-        <input type="text" name='phonenumber' value={kund.phonenumber} onChange={(e) => handleChange(e)}></input>
+      <label>Telefonnummer: </label>
+      <input
+        type="text"
+        name="phoneNumber"
+        value={kund.phoneNumber}
+        onChange={(e) => handleChange(e)}
+      ></input>
 
-        <label>fastighetsbeteckning: </label>
-        <input type="text" name='fastighetsbeteckning' value={kund.fastighetsbeteckning} onChange={(e) => handleChange(e)}></input>
+      <label>fastighetsbeteckning: </label>
+      <input
+        type="text"
+        name="propertyDesignation"
+        value={kund.propertyDesignation}
+        onChange={(e) => handleChange(e)}
+      ></input>
 
-        <label>Personnummer: </label>
-        <input type="text" name='personnr' value={kund.personnr} onChange={(e) => handleChange(e)}></input>
-        
-        <button onClick={saveKund}>Spara</button>
+      <label>Personnummer: </label>
+      <input
+        type="text"
+        name="socialSecurityNumber"
+        value={kund.socialSecurityNumber}
+        onChange={(e) => handleChange(e)}
+      ></input>
+
+      <button onClick={saveKund}>Spara</button>
     </div>
-  )
-}
+  );
+};
 
-export default AddCustomer
+export default AddCustomer;
--- construction-app/client/src/components/CustomerRegister.js
+++ construction-app/client/src/components/CustomerRegister.js
@@ -1,68 +1,82 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import ApiConnector from '../services/ApiConnector';
-import '../styles/CustomerRegister.css';
-import { FaPen } from 'react-icons/fa';
-import { ImCross } from 'react-icons/im';
-import { BsPerson } from 'react-icons/bs';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiConnector from "../services/ApiConnector";
+import "../styles/CustomerRegister.css";
+import { FaPen } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { BsPerson } from "react-icons/bs";
 
 const CustomerRegister = () => {
-    // Declaring variables
-    const navigate = useNavigate();
-    const [kunder, setKunder] = useState(null);
-    const [loading, setLoading] = useState(true);
+  // Declaring variables
+  const navigate = useNavigate();
+  const [kunder, setKunder] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        // Gets all the clients on page load
-        const fetData = async () => {
-            setLoading(true);
-            try {
-                const response = await ApiConnector.getKund();
-                setKunder(response.data)
-            } catch (error) {
-                console.log(error)
-            }
-            setLoading(false)
-        }
-        fetData();
-    }, []);
+  useEffect(() => {
+    // Gets all the clients on page load
+    const fetData = async () => {
+      setLoading(true);
+      try {
+        const response = await ApiConnector.getKund();
+        setKunder(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetData();
+  }, []);
 
   return (
-    <div className='container'>
-        <div className='content'>
-            <div className='topContent'>
-                <input type="text" className="myInput" placeholder="Sök efter namn.." title="Type in a name"></input>
-                <button className='addClient' onClick={() => navigate("/skapakund")}><span className='newClientIcon'><BsPerson /></span> Ny kund</button>
-            </div>
-        <table className="styled-table">
-            <thead>
-                <tr>
-                    <th>Namn</th>
-                    <th>Adress</th>
-                    <th>Kommentar</th>
-                    <th>Skapad</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            {!loading && (
-                <tbody>
-                    {kunder.map((kunder) => (
-                        <tr key={kunder.id}>
-                            <td>{kunder.name}</td>
-                            <td>{kunder.address}</td>
-                            <td>Fönster</td>
-                            <td>2022-10-04</td>
-                            <td className='icons'><FaPen className='editIcon' /></td>
-                            <td className='icons'><ImCross className='removeIcon' /></td>
-                        </tr>
-                    ))}
-                </tbody>
-            )}
-        </table>
+    <div className="container">
+      <div className="content">
+        <div className="topContent">
+          <input
+            type="text"
+            className="myInput"
+            placeholder="Sök efter namn.."
+            title="Type in a name"
+          ></input>
+          <button className="addClient" onClick={() => navigate("/skapakund")}>
+            <span className="newClientIcon">
+              <BsPerson />
+            </span>{" "}
+            Ny kund
+          </button>
         </div>
+        <table className="styled-table">
+          <thead>
+            <tr>
+              <th>Namn</th>
+              <th>Adress</th>
+              <th>Kommentar</th>
+              <th>Skapad</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          {!loading && (
+            <tbody>
+              {kunder.map((kunder) => (
+                <tr key={kunder.id}>
+                  <td>{kunder.name}</td>
+                  <td>{kunder.address}</td>
+                  <td>Fönster</td>
+                  <td>2022-10-04</td>
+                  <td className="icons">
+                    <FaPen className="editIcon" />
+                  </td>
+                  <td className="icons">
+                    <ImCross className="removeIcon" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CustomerRegister
+export default CustomerRegister;
