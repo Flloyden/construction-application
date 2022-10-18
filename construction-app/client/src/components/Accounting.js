af5a7be8cf9f6c2ec7ffbdee7a126146448e039c
Mads Nörklit Johansen <31743570+Madzic8@users.noreply.github.com>
2022-10-18 16:36:14 +0200

    + Added connection to accounting table in ApiConnectior.js
    + Small change on Bokföring GUI

--- construction-app/client/src/components/Accounting.js
+++ construction-app/client/src/components/Accounting.js
@@ -5,6 +5,7 @@ export default function Accounting() {
   return (
     <div className='container'>
         <h1>Bokföring</h1>
+        <p>Varför är react så svårt</p>
     </div>
   )
 }
--- construction-app/client/src/services/ApiConnector.js
+++ construction-app/client/src/services/ApiConnector.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // Base url to get api
 const KUND_API_BASE_URL = "http://localhost:8080/api/v1/kunder"
+const ACCOUNTING_API_BASE_URL = "http://localhost:8080/api/v1/bokföring"
 
 class ApiConnector {
     //Saves the "kund" to the database
@@ -22,6 +23,31 @@ class ApiConnector {
     deleteKund(kund) {
         //Deletes a kund from the database
         return axios.delete(KUND_API_BASE_URL + "/" + kund + "/remove");
+    }
+
+    // ACCOUNTING / WARRANTY
+    saveWarranty(warranty)
+    {
+        //Saves the warranty to the database (Can also update existing warranty if ID already exists)
+        return axios.post(ACCOUNTING_API_BASE_URL, warranty);
+    }
+
+    getWarranties()
+    {
+        //Gets existing warranties from the database
+        return axios.get(ACCOUNTING_API_BASE_URL);
+    }
+
+    getWarranty(warranty)
+    {
+        //Gets specific warranty from the database
+        return axios.get(ACCOUNTING_API_BASE_URL + "/" + warranty)
+    }
+
+    deleteWarranty(warranty)
+    {
+        //Deletes an existing warranty from the database
+        return axios.delete(ACCOUNTING_API_BASE_URL + "/" + warranty + "/remove")
     }
 }
 
