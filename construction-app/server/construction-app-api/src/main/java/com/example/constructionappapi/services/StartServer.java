a2c403a15668d4828d122f6606823e2cd83e399f
Mads Nörklit Johansen <madskingjohansen@outlook.com>
2022-10-18 13:36:00 +0200

    la till möjlighet att executea task varje dygn, t.ex kolla garatin osv

--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/StartServer.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/StartServer.java
@@ -4,12 +4,29 @@ import com.sun.jdi.event.ExceptionEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @SpringBootApplication
 public class StartServer {
     public static void main(String[] args) {
         try{
             SpringApplication.run(StartServer.class, args);
             System.out.println("Server is running!");
+
+
+                TimerTask repeatedTask = new TimerTask() {
+                    public void run() {
+                        System.out.println("Task performed on " + new Date());
+                    }
+                };
+                Timer timer = new Timer("Timer");
+
+                long delay = 1000L;
+                long period = 1000L * 60L * 60L * 24L; //Utför task varje 24h
+                timer.scheduleAtFixedRate(repeatedTask, delay, period);
+
         }catch (Exception e){
             System.out.println("Spring application could not run: " + e);
         }
