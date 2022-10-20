fab6ea04137bcfad40aa2664cbeeacebc3f653d7
ZhinkyPinky <jonathan.engstrom@gmail.com>
2022-10-20 15:25:21 +0200

    +removal of work from the calendar possibly working

--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/businessLogicLayer/Calendar.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/businessLogicLayer/Calendar.java
@@ -16,59 +16,38 @@ public class Calendar {
     public HashMap<CalendarEntity, WorkEntity> calendarDates = new HashMap<>();
 
     public Calendar() {
-        addWork(new WorkEntity(
-                1,
-                "Door",
-                LocalDate.of(2023, 5, 22),
-                10,
-                "testNote",
-                null, Status.NOTSTARTED,
-                null,
-                new ArrayList<>()));
+        WorkEntity door = new WorkEntity(1, "Door", LocalDate.of(2023, 5, 22), 10, "testNote", null, Status.NOTSTARTED, null, new ArrayList<>());
+        WorkEntity fence = new WorkEntity(2, "Fence", LocalDate.of(2023, 5, 25), 10, "testNote", null, Status.NOTSTARTED, null, new ArrayList<>());
+        WorkEntity roof = new WorkEntity(3, "Roof", LocalDate.of(2023, 5, 29), 2, "testNote", null, Status.NOTSTARTED, null, new ArrayList<>());
 
-        addWork(new WorkEntity(
-                2,
-                "Fence",
-                LocalDate.of(2023, 5, 25),
-                10,
-                "testNote",
-                null, Status.NOTSTARTED,
-                null,
-                new ArrayList<>()));
+        addWork(fence);
+        addWork(door);
+        addWork(roof);
 
-        addWork(new WorkEntity(
-                2,
-                "Roof",
-                LocalDate.of(2023, 5, 27),
-                2,
-                "testNote",
-                null, Status.NOTSTARTED,
-                null,
-                new ArrayList<>()));
+        System.out.println();
+        System.out.println("Add");
+        printCalendar();
 
+        removeWork(door);
+        System.out.println("Remove");
+        printCalendar();
+
+        removeWork(roof);
+        System.out.println("Remove");
         printCalendar();
     }
 
-    public Calendar addWork(WorkEntity work) {
+    public void addWork(WorkEntity work) {
         int daysToAdd = work.getNumberOfDays();
         int daysToShuffleForward = daysToAdd;
-        /*
-        int startDate = work.getStartDate().getDayOfYear();
-        int startYear = work.getStartDate().getYear();
-        int startMonth = work.getStartDate().getMonthValue();
-        int startDay = work.getStartDate().getDayOfMonth();
-
-         */
 
         for (int i = 0; i < daysToAdd; i++) {
             LocalDate date = work.getStartDate().plusDays(i);
-            //LocalDate.of(startYear, startMonth, startDay).plusDays(i);
             CalendarEntity calendarEntity = new CalendarEntity(date, work);
 
             /*If the date where the new work is getting added already contains something the content that is already there needs
               to be shuffled forward x days decided by the daysToShuffleForward variable.*/
-            if (calendarDates.get(new CalendarEntity(date)) != null)
-                shuffleForward(date, daysToShuffleForward);
+            if (calendarDates.get(new CalendarEntity(date)) != null) shuffleForward(date, daysToShuffleForward);
             else daysToShuffleForward--;/*If the spot where new work is being added is free all future work that needs to be
             shuffled forward should be shuffled forward one day less.*/
 
@@ -76,69 +55,61 @@ public class Calendar {
             work.getCalendar().add(calendarEntity);
             calendarDates.put(calendarEntity, work);
         }
-
-        return this;
     }
 
     public void shuffleForward(LocalDate date, int daysToShuffle) {
         LocalDate newDate = date.plusDays(daysToShuffle);
-        //LocalDate.ofYearDay(LocalDate.now().getYear(), dayOfYear + daysToShuffle);
         /*If the date where the work-object is trying to get shuffled to is already taken, the work-object
         that is already there should be shuffled forward one day.*/
         if (calendarDates.containsKey(new CalendarEntity(newDate))) shuffleForward(newDate, 1);
 
         /*If the date is free the work-object that is getting shuffled forward can be added. */
         WorkEntity workToAdd = calendarDates.get(new CalendarEntity(date));
-        if (workToAdd == null) System.out.println("beep: null");
         calendarDates.put(new CalendarEntity(newDate, workToAdd), workToAdd);
     }
 
     public void removeWork(WorkEntity work) {
-        calendarDates.entrySet().removeIf(item -> item.getValue() == work);
+        //Remove all map entries containing a work-object that has the same id as the specified work-object.
+        calendarDates.entrySet().removeIf(item -> item.getValue().getId() == work.getId());
 
+        //A work-item being moved back shouldn't be moved back past the date that the last work-item that was moved back were moved to.
         LocalDate[] lastDateMovedTo = {LocalDate.MIN};
         calendarDates.keySet().stream().sorted().forEach(calendarEntity -> {
             if (calendarEntity.getDate().isAfter(work.getStartDate())) {
                 lastDateMovedTo[0] = moveCalendarItemBackwards(calendarDates.get(calendarEntity), calendarEntity, lastDateMovedTo[0]);
             }
         });
-
-        System.out.println();
     }
 
     public LocalDate moveCalendarItemBackwards(WorkEntity workToMove, CalendarEntity calendarEntity, LocalDate lastDateMovedTo) {
-        LocalDate dateToMoveTo = calendarEntity.getDate().minusDays(Long.parseLong("1"));
-        LocalDate freeSpot = calendarEntity.getDate();
+        LocalDate possibleDate = calendarEntity.getDate().minusDays(1L);
+        LocalDate freeCalendarSpot = null;
 
-        //As long as
         while (true) {
             //Stop if we're past the last date that a work item was moved to.
-            if (!dateToMoveTo.isAfter(lastDateMovedTo)) break;
+            if (!possibleDate.isAfter(lastDateMovedTo)) break;
             //Stop if start date of work being moved is reached.
-            if (workToMove.getStartDate().isAfter(dateToMoveTo)) break;
+            if (workToMove.getStartDate().isAfter(possibleDate)) break;
             //Stop if the work being moved is the same as the one for which the date is being checked.
-            if (calendarDates.get(dateToMoveTo) == workToMove) break;
+            if (calendarDates.get(new CalendarEntity(possibleDate)) == workToMove) break;
             //Stop if reached today's date.
-            if (LocalDate.now().isEqual(dateToMoveTo)) break;
+            if (LocalDate.now().isEqual(possibleDate)) break;
 
-            //System.out.println("oldDate: " + dateToMove);
-            //System.out.println("newDate: " + dateToMoveTo);
-            dateToMoveTo = dateToMoveTo.minusDays(1L);
-
-            if (!calendarDates.containsKey(dateToMoveTo)) {
-                if (dateToMoveTo.getDayOfWeek() != DayOfWeek.SATURDAY && dateToMoveTo.getDayOfWeek() != DayOfWeek.SUNDAY) {
-                    freeSpot = dateToMoveTo;
+            //Set freeCalenderSpot to possibleDate if the spot in the calendar is free, and it's not on a weekend.
+            if (!calendarDates.containsKey(new CalendarEntity(possibleDate))) {
+                if (possibleDate.getDayOfWeek() != DayOfWeek.SATURDAY && possibleDate.getDayOfWeek() != DayOfWeek.SUNDAY) {
+                    freeCalendarSpot = possibleDate;
                 }
             }
-        }
-        dateToMoveTo = dateToMoveTo.plusDays(1L);
-        //System.out.println(date + "->" + dateToMoveTo);
 
-        if (!calendarDates.containsKey(freeSpot)) {
-            calendarDates.put(new CalendarEntity(freeSpot, workToMove), workToMove);
+            possibleDate = possibleDate.minusDays(1L);
+        }
+
+        //Insert
+        if (freeCalendarSpot != null && !calendarDates.containsKey(new CalendarEntity(freeCalendarSpot))) {
+            calendarDates.put(new CalendarEntity(freeCalendarSpot, workToMove), workToMove);
             calendarDates.remove(calendarEntity);
-            //System.out.println("boop" + dateToMoveTo);
-            return freeSpot;
+            return freeCalendarSpot;
         }
 
         return lastDateMovedTo;
@@ -147,11 +118,11 @@ public class Calendar {
     public void printCalendar() {
         calendarDates.keySet().stream().sorted(Comparator.reverseOrder()).forEach(key -> {
             WorkEntity work = calendarDates.get(key);
-            System.out.print("Key: " + key.getDate() + " : " + key.hashCode());
-            if (work != null)
-                System.out.println(" Value: " + work.getName() + " : " + work.getStartDate() + " : " + work.getNumberOfDays());
-            else System.out.println(" Value: null");
+            String s = String.format("Key: %s %-10s %s | Value: %-6s %s %s", key.getDate(), key.getDate().getDayOfWeek(), key.hashCode(), work.getName(), work.getStartDate(), work.getNumberOfDays());
+            System.out.println(s);
         });
+
+        System.out.println();
     }
 }
 
