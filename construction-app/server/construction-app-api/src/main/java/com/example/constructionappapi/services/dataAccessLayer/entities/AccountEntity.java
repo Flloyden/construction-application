dc9b37eb0f73e6d87bd9c56b2cb59cd941a69745
ZhinkyPinky <jonathan.engstrom@gmail.com>
2022-11-25 23:17:31 +0100

    Stuff

--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/dataAccessLayer/entities/AccountEntity.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/dataAccessLayer/entities/AccountEntity.java
@@ -3,8 +3,11 @@ package com.example.constructionappapi.services.dataAccessLayer.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Table(name = "account")
@@ -14,10 +17,35 @@ import javax.persistence.*;
 /**
  * A class creating and giving access to the table account in DB
  */
-public class AccountEntity {
+public class AccountEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String username;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
