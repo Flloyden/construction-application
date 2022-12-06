87c257ee00c0b3e457403e819a4a9c7549e996f1
Adam Lloyd <lloyd.adam99@gmail.com>
2022-12-06 22:44:24 +0100

    Error page

--- construction-app/client/src/components/Error.js
+++ construction-app/client/src/components/Error.js
@@ -4,9 +4,15 @@ import { NavLink } from 'react-router-dom';
 export default function Error() {
    
   return (
-    <div className='container'>
-      <h1>Error! Sidan du söker finns inte eller så behöver du logga in för att få åtkomst till den.</h1>
-      <NavLink to='/'>Hem</NavLink>
+    <div className='text 2x1 font-semibold flex-1 h-screen'>
+      <div className='w-full'>
+      <img src='404.png' alt='Error img' className='w-full' />
+      </div>
+      <div className='text-center mt-10'>
+      <h1 className='text-4xl'>SIDAN KUNDE INTE HITTAS</h1>
+      <p className='mt-6 text-2xl font-light'>Kaninerna har knaprat i kablarna igen...</p>
+      <p className='mt-2 text-2xl font-light'>Kanske hjälper detta: <span className='underline text-blue-800'><NavLink to='/'>Hem</NavLink></span></p>
+      </div>
     </div>
   )
 }
