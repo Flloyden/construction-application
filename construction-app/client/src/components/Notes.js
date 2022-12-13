08d75fd680779f47aaf1e93b2e415467ee8ff7ae
Adam Lloyd <lloyd.adam99@gmail.com>
2022-12-13 18:15:33 +0100

    removed code that was not used

--- construction-app/client/src/components/Notes.js
+++ construction-app/client/src/components/Notes.js
@@ -13,15 +13,7 @@ export default function Notes(props) {
     document.documentElement.classList.remove("dark");
   }
   const copy = [...props.currentCustomer.customerNotes];
-  const resultSum = copy.filter((item) => item.noteStatus === "SUMMARIZED");
   const resultReg = copy.filter((item) => item.noteStatus === "NOTSUMMARIZED");
-  const selectedOnes = resultSum.map((item) => {
-    return item.workNumber;
-  });
-  
-  const result = resultReg.filter((col) => {
-    return !selectedOnes.find((selected) => selected === col.workNumber);
-  });
 
     return (
       <div>
