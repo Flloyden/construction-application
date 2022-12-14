dfa67d0994033c2df222b5536eeb59dea1db3eee
Adam Lloyd <lloyd.adam99@gmail.com>
2022-12-14 17:23:31 +0100

    fixed to only accept pdf

--- construction-app/client/src/components/ChangeWorkInfo.js
+++ construction-app/client/src/components/ChangeWorkInfo.js
@@ -162,7 +162,7 @@ const ChangeWorkInfo = (props) => {
               className="rounded block w-full p-2.5 border-gray-500 border text-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
               type="file"
               name="offer"
-              accept="image/png, image/jpg, image/jpeg, application/pdf"
+              accept="application/pdf"
               onChange={(item) => {
                 handleFile(item);
               }}
