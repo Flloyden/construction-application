f8b09009560e3fa4256536eb203e3f922ae83fc5
Adam Lloyd <lloyd.adam99@gmail.com>
2022-12-21 11:44:00 +0100

    Set color for calendar

--- construction-app/client/src/components/SettingsCalendar.js
+++ construction-app/client/src/components/SettingsCalendar.js
@@ -1,21 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
+
 export default function SettingsCalendar() {
+  const [colorWork, setColorWork] = useState("#1d4ed8");
+  const [showColorWork, setShowColorWork] = useState(false);
+
+  const [colorSemester, setColorSemester] = useState("#15803d");
+  const [showColorSemester, setShowColorSemester] = useState(false);
+
+  const [colorWeekend, setColorWeekend] = useState("#b91c1c");
+  const [showColorWeekend, setShowColorWeekend] = useState(false);
+
   return (
     <div className="w-48 mt-10">
       <h1 className="text-2xl">Kalender</h1>
       <div className="flex w-full justify-between pt-4">
         <h1>Färg på jobb</h1>
-        <div className="w-5 h-5 bg-blue-700 rounded"></div>
+        <button
+          className="w-6 h-6 rounded border"
+          style={{ backgroundColor: colorWork }}
+          onClick={() => setShowColorWork((showColor) => !showColor)}
+        ></button>
+        {showColorWork && (
+          <div className="absolute ml-60">
+            <SketchPicker
+              color={colorWork}
+              onChange={(updatedColor) => setColorWork(updatedColor.hex)}
+            />
+            <button className="bg-blue-600 rounded text-white hover:bg-blue-500 font-bold py-2 px-4 w-full duration-300" onClick={() => setShowColorWork((showColorWork) => !showColorWork)}>Ok</button>
+          </div>
+        )}
       </div>
 
       <div className="flex mt-2 w-full justify-between">
-      <h1>Färg på Semester</h1>
-        <div className="w-5 h-5 bg-green-700 rounded"></div>
+        <h1>Färg på Semester</h1>
+        <button
+          className="w-6 h-6 rounded border"
+          style={{ backgroundColor: colorSemester }}
+          onClick={() => setShowColorSemester((showColorSemester) => !showColorSemester)}
+        ></button>
+        {showColorSemester && (
+          <div className="absolute ml-60">
+            <SketchPicker
+              color={colorSemester}
+              onChange={(updatedColor) => setColorSemester(updatedColor.hex)}
+            />
+            <button className="bg-blue-600 rounded text-white hover:bg-blue-500 font-bold py-2 px-4 w-full duration-300" onClick={() => setShowColorSemester((showColorSemester) => !showColorSemester)}>Ok</button>
+          </div>
+        )}
       </div>
 
       <div className="flex mt-2 w-full justify-between">
-      <h1>Färg på helgdagar</h1>
-        <div className="w-5 h-5 bg-red-700 rounded"></div>
+        <h1>Färg på helgdagar</h1>
+        <button
+          className="w-6 h-6 rounded border"
+          style={{ backgroundColor: colorWeekend }}
+          onClick={() => setShowColorWeekend((showColorWeekend) => !showColorWeekend)}
+        ></button>
+        {showColorWeekend && (
+          <div className="absolute ml-60">
+            <SketchPicker
+              color={colorWeekend}
+              onChange={(updatedColor) => setColorWeekend(updatedColor.hex)}
+            />
+            <button className="bg-blue-600 rounded text-white hover:bg-blue-500 font-bold py-2 px-4 w-full duration-300" onClick={() => setShowColorWeekend((showColorWeekend) => !showColorWeekend)}>Ok</button>
+          </div>
+        )}
       </div>
     </div>
   );
