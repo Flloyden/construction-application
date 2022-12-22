75ebf1b47a7e698cac2d4de16e151f19f0e0b6c8
ZhinkyPinky <jonathan.engstrom@gmail.com>
2022-12-22 15:01:22 +0100

    +profile image + email for user

--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/presentationLayer/AccountAPI.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/presentationLayer/AccountAPI.java
@@ -36,6 +36,36 @@ public class AccountAPI {
             s.append("\"user\":").append("{");
             s.append("\"id\":").append(account.getId()).append(",");
             s.append("\"username\":").append("\"").append(account.getUsername()).append("\"");
+            s.append("\"email\":").append("\"").append(account.getEmail()).append("\"");
+            s.append("\"profileImage\":").append("\"").append(account.getProfileImage()).append("\"");
+            s.append("}");
+            s.append("}");
+        } else {
+            s.append("{");
+            s.append("\"status\":").append("\"error\"").append(",");
+            s.append("\"message\":").append("\"Felaktig inloggning\"");
+            s.append("}");
+        }
+
+        return s.toString();
+    }
+
+    @GetMapping("/user")
+    public String getUserInfo(@RequestBody AccountEntity account) {
+        Optional<AccountEntity> accountEntity = accountRepository.findFirstByUsernameAndPassword(account.getUsername(), account.getPassword());
+        System.out.println(account);
+        StringBuilder s = new StringBuilder();
+
+        if(accountEntity.isPresent()) {
+            s.append("{");
+            s.append("\"status\":").append("\"ok\"").append(",");
+            s.append("\"message\":").append("\"Logged in\"").append(",");
+            s.append("\"accessToken\":").append("\"").append(UUID.randomUUID()).append("\"").append(",");
+            s.append("\"user\":").append("{");
+            s.append("\"id\":").append(account.getId()).append(",");
+            s.append("\"username\":").append("\"").append(account.getUsername()).append("\"");
+            s.append("\"email\":").append("\"").append(account.getEmail()).append("\"");
+            s.append("\"profileImage\":").append("\"").append(account.getProfileImage()).append("\"");
             s.append("}");
             s.append("}");
         } else {
