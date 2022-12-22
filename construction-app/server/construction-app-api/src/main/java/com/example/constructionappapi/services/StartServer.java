832dbe0d8bd14e8e0f99b3b373c6257ac9abfa1a
ZhinkyPinky <jonathan.engstrom@gmail.com>
2022-12-22 17:24:54 +0100

    +default img for user

--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/StartServer.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/StartServer.java
@@ -3,7 +3,6 @@ package com.example.constructionappapi.services;
 import com.example.constructionappapi.services.businessLogicLayer.Calendar;
 import com.example.constructionappapi.services.businessLogicLayer.CalendarSingleton;
 import com.example.constructionappapi.services.businessLogicLayer.repositories.AccountRepository;
-import com.example.constructionappapi.services.businessLogicLayer.repositories.WorkRepository;
 import com.example.constructionappapi.services.dataAccessLayer.entities.AccountEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +12,6 @@ import java.util.Date;
 import java.util.Optional;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class StartServer {
@@ -46,7 +42,12 @@ public class StartServer {
 
             //TODO: Remove when done with project.
             //Adds an account to the database on server-start to make testing easier.
-            AccountEntity accountEntity = new AccountEntity(0, "admin", "admin", "", "admin@admin.com");
+            AccountEntity accountEntity = new AccountEntity();
+            accountEntity.setId(0);
+            accountEntity.setUsername("admin");
+            accountEntity.setPassword("admin");
+            accountEntity.setEmail("admin@admin.com");
+            //new AccountEntity(0, "admin", "admin", null, "admin@admin.com");
             AccountRepository accountRepository = configurableApplicationContext.getBean(AccountRepository.class);
             Optional<AccountEntity> accountEntityOptional = accountRepository.findFirstByUsernameAndPassword("admin", "admin");
             if (accountEntityOptional.isEmpty()) {
