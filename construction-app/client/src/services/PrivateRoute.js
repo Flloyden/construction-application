b90c9c6632311eb698b94c982b6066703b2ef00e
Adam Lloyd <lloyd.adam99@gmail.com>
2022-12-27 15:38:17 +0100

    Update PrivateRoute.js

--- construction-app/client/src/services/PrivateRoute.js
+++ construction-app/client/src/services/PrivateRoute.js
@@ -4,10 +4,19 @@ const PrivateRoutes = () => {
   // Gets the key in localstorage
   const getValue = localStorage.getItem('accessToken');
   let auth = { token: getValue };
+  console.log(auth)
+
+  function checkToken() {
+    if (!auth.token) {
+      return <Navigate to={"/login"} replace />
+    } else {
+      return <Outlet />
+    }
+  }
 
   return (
     // Navigates the user to the right page if logged in or not
-    auth.token ? <Outlet /> : <Navigate to="/login" state={{ auth }} />
+    checkToken()
   );
 };
 
