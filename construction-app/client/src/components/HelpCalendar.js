cc0323d33d63a4c89de8da931c74914c58cfd7c5
Mads Nörklit Johansen <31743570+Madzic8@users.noreply.github.com>
2022-12-27 14:02:02 +0100

    +help

--- construction-app/client/src/components/HelpCalendar.js
+++ construction-app/client/src/components/HelpCalendar.js
@@ -7,7 +7,35 @@ export default function HelpCalendar() {
         <div className="w-fit">
           <div className="">
             <h1 className="font-bold">Kalender</h1>
+            <p className="font-normal">
+              Till höger ser man vår kalender. Här kan man se alla jobb, semestrar, helger samt röda dagar. 
+            </p>
+            <p className="mt-4">Klicka på jobb</p>
+            <p className="font-normal">
+              Genom att trycka på kalenderns olika dagar så kan man göra vissa funktioner.
+              Trycker du på de blåa dagarna vilekt motsvarar jobb, så får du en "pop-up" som visas på bild 3 där du kan välja om du vill hoppa till jobbets motsvarande kund.
+            </p>
+            <p className="mt-4">Klicka på semester</p>
+            <p className="font-normal">
+              Genom att trycka på de gröna dagarna vilket motsvarar inlagd semester, så får du en "pop-up" som gick dig möjligheten att ändra semestern. Det visas ett exempel på bild 2.
+              </p>
+            
+              <p className="mt-4">Klicka på blanka eller röda dagar</p>
+              <p className="font-normal">
+              Genom att trycka på de röda eller "blanka" dagarna bemöts du av en "pop-up" som kommer berätta att det inte finns något att göra den dagen. På de blanka är det för att det inte finns
+              ett aktivt jobb den dagen och de röda dagarna motsvarar "röda" dagar.
+              </p>
+
+              <p className="mt-4">Klicka på pilarna uppe i hörnen</p>
+              <p className="font-normal">
+              Genom att trycka på pilarna uppe i hörnen, så hoppar kalendern fram eller back 6 månader åt gången beroende på vilken pil som trycks på. 
+              </p>
           </div>
+        </div>
+        <div className="">
+          <img src="kalender.png" alt="Översikt bild" className="border-black border rounded" />
+          <img src="redigera_semester.png" alt="Översikt bild" className="border-black border rounded mt-2" />
+          <img src="hoppatillkund.png" alt="Översikt bild" className="border-black border rounded mt-2" />
         </div>
       </div>
     </div>
--- construction-app/client/src/components/HelpWarranty.js
+++ construction-app/client/src/components/HelpWarranty.js
@@ -7,7 +7,39 @@ export default function HelpWarranty() {
         <div className="w-fit">
           <div className="">
             <h1 className="font-bold">Garantier</h1>
+            <p>Garantiregister</p>
+            <p className="font-normal">
+              Till höger ser man vårt garantiregister. I detta register har du möjlighet att söka efter utgående garantier beroende på namn, skapa en ny garanti och radera existerande garanti.
+            </p>
+            <p className="mt-4">Automatisk radering</p>
+            <p className="font-normal">
+              Vårt program BiTs kollar dagligen ifall några av dina garantier har gått ut. Ifall deras datum är förbipasserat, då raderas garantin från listan.
+              På översikten av programmet kan du se nästa utgående garanti så man inte blir överraskad.
+            </p>
+
+            <p className="mt-4">Lägg till en ny garanti</p>
+            <p className="font-normal">
+                Du har möjligheten att lägga till en ny garanti genom att klicka på den blåa knappen uppe till höger.
+               
+              </p>
+
+            <p className="mt-4">Redigera och radera en garanti</p>
+            <p className="font-normal">
+                Du har möjligheten att redigera en existerande garanti genom att klicka in på den. 
+                Du har också möjlighet att ta bort garantin manuellt genom att klicka på soptunnan längst till höger.
+              </p>
+              <p className="font-bold">
+              (OBS. Lägg märke till att en raderad garanti inte ligger kvar och måste återskaps med "Ny garanti" knappen)
+              </p>      
+
+            <p className="mt-4">Visa kvitto</p>
+            <p className="font-normal">
+               Genom att klicka på knappen "Visa kvitto" så visas det kvittot som sparades när du skapade garantin.
+              </p> 
           </div>
+        </div>
+        <div className="">
+          <img src="garantier.png" alt="Översikt bild" className="border-black border rounded" />
         </div>
       </div>
     </div>
