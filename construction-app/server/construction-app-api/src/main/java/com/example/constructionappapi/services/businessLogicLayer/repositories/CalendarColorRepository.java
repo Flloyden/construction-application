b7b5b75378f90c08d0a711ea34314c2f0ec71d4e
Mads Nörklit Johansen <31743570+Madzic8@users.noreply.github.com>
2023-01-03 12:09:52 +0100

    +fix

--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/businessLogicLayer/repositories/CalendarColorRepository.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/businessLogicLayer/repositories/CalendarColorRepository.java
@@ -2,13 +2,15 @@ package com.example.constructionappapi.services.businessLogicLayer.repositories;
 
 import com.example.constructionappapi.services.dataAccessLayer.dao.CalendarColorDao;
 import com.example.constructionappapi.services.dataAccessLayer.entities.CalendarColorEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class CalendarColorRepository {
-
+    @Autowired
     CalendarColorDao calendarColorDao;
 
     public ResponseEntity<CalendarColorEntity> createCalendarcolor(CalendarColorEntity calendarColor)
--- construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/presentationLayer/CalendarColorAPI.java
+++ construction-app/server/construction-app-api/src/main/java/com/example/constructionappapi/services/presentationLayer/CalendarColorAPI.java
@@ -5,10 +5,9 @@ import com.example.constructionappapi.services.dataAccessLayer.entities.Calendar
 import com.example.constructionappapi.services.dataAccessLayer.entities.CalendarEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -22,4 +21,9 @@ public class CalendarColorAPI {
         return calendarColorRepository.createCalendarcolor(calendarColorEntity);
     }
 
+    @GetMapping("/calendarcolor")
+    public ResponseEntity<CalendarColorEntity> getCalendarColor(@PathVariable final Long id)
+    {
+        return calendarColorRepository.getColorEntity();
+    }
 }
