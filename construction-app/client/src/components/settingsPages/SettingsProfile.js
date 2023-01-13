36f6f42f12a87db324e9aaf4bc3a4bed8ae09d50
Susanne Vikström <76662096+SusVik@users.noreply.github.com>
2023-01-13 14:10:04 +0100

    error handling password

--- construction-app/client/src/components/settingsPages/SettingsProfile.js
+++ construction-app/client/src/components/settingsPages/SettingsProfile.js
@@ -155,7 +155,7 @@ export default function SettingsProfile() {
                       className="rounded block w-full p-1 border-gray-500 border text-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
                       type="password"
                       name="password"
-                      minLength={8}
+                      minLength={1}
                       required
                       value={newUserInfo.password}
                       onChange={handleChange}
