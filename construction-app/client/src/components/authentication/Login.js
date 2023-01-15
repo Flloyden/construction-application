1e6753c74d65cffdc7280c9b65ad1fd4c9aa7828
Adam Lloyd <lloyd.adam99@gmail.com>
2023-01-15 14:06:44 +0100

    fixed type

--- construction-app/client/src/components/authentication/Login.js
+++ construction-app/client/src/components/authentication/Login.js
@@ -131,7 +131,7 @@ const Login = () => {
                       email: e.target.value,
                     })
                   }
-                  type="text"
+                  type="email"
                   placeholder="Email"
                   className="rounded block w-full p-2.5 border-gray-500 border text-black focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
                 />
